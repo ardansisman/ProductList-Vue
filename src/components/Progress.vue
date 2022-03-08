fa74a89b60c70a8bc6ba05ff390c68e3604f3440
ardansisman <ardann.68@gmail.com>
2022-03-08 22:50:37 +0300

    --firstCommit

---
+++ src/components/Progress.vue
@@ -0,0 +1,14 @@
+<template>
+     <div class="progress">
+      <div ref="progressbar" class="progress-bar bg-info" role="progressbar" style="width: 0%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+        0/10
+      </div>
+    </div>
+</template>
+
+<script>
+import eventBus from "../main";
+export default {
+ 
+}
+</script>
